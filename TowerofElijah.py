4120cc80bbc39c241a5441f762d3d028ceb542c8
elijahnt <40532634+elijahnt@users.noreply.github.com>
2018-06-23 22:14:13 -0600

    Update TowerofElijah.py

--- TowerofElijah.py
+++ TowerofElijah.py
@@ -1,4 +1,10 @@
 import random
 import time
 
-print("Hello World")
+def displayIntro():
+  print("It's been 16 years since the start of the Demon- Human War"
+  print("and the humans are barely holding on at this point."
+  print("You, however, are what they are holding onto."
+  print("Your name is Elijah Not, and you are the offspring of an Angel"
+  print("and a Human. Angels died shortly after the war started, and you lost")
+  print("your mom at the rough age of 4.")
